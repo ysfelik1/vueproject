00af00f4f63f77dd121bb78bb8d0862ef956e3d5
Yusuf Elik <ysfelik1@gmail.com>
2023-10-17 00:02:24 +0200

    render data from list

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,24 +1,62 @@
 <template>
-  <div v-bind:id="customerId">
+ <!--  <div v-bind:id="customerId">
     <div>{{ name }}</div>
     <div v-text="lastname"></div>
     <div v-text="email"></div>
     <button v-bind:disabled="isDisable">select</button>
   </div>
+  -->
+  <div v-for="customer in customersList" :key="customer.customerId">
+    <div>{{ customer.name }}</div>
+    <div v-text="customer.lastname"></div>
+    <div v-text="customer.email"></div>
+    <button v-bind:disabled="customer.isDisable">select</button>
+  </div>
 </template>
 
-<script>
 
+<script>
 
 export default {
   name: 'App',
   data(){
     return {
-      customerId:'1',
-      name:'Yusuf',
-      lastname:'ELIK',
-      email: 'ysfelik1@gmail.com',
-      isDisable:false,
+      customersList: [{
+        customerId: '1',
+        name: 'Yusuf',
+        lastname: 'ELIK',
+        age:34,
+        email: 'ysfelik1@gmail.com',
+        isDisable: false,
+      }
+        ,
+      {
+        customerId: '2',
+        name: 'John',
+        lastname: 'Doe',
+        age:55,
+        email: 'jdoe@gmail.com',
+        isDisable: false,
+      }   
+       ,
+      {
+        customerId: '3',
+        name: 'Elon',
+        lastname: 'Musk',
+        age:54,
+        email: 'emusk@gmail.com',
+        isDisable: false,
+      }
+      ,
+      {
+        customerId: '4',
+        name: 'Bill',
+        lastname: 'Gates',
+        age:64,
+        email: 'bgates@gmail.com',
+        isDisable: false,
+      }
+      ]
     }
   }
   
