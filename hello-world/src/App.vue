31740c9d1556dea0afd7f6631c42db07ae4adbfd
Yusuf Elik <ysfelik1@gmail.com>
2023-10-19 00:31:10 +0200

    align everything center

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,37 +1,37 @@
 <template>
- <!--  <div v-bind:id="customerId">
+  <!--  <div v-bind:id="customerId">
     <div>{{ name }}</div>
     <div v-text="lastname"></div>
     <div v-text="email"></div>
     <button v-bind:disabled="isDisable">select</button>
   </div>
   -->
-  <div v-for="customer in customersList" :key="customer.customerId">
+  <div class="list" v-for="customer in customersList" :key="customer.customerId">
     <div>{{ customer.name }}</div>
     <div v-text="customer.lastname"></div>
     <div v-text="customer.email"></div>
     <button v-bind:disabled="customer.isDisable">select</button>
   </div>
-  <FormComponent/>
+  <FormComponent />
 </template>
 
 
 <script>
- import FormComponent from './components/form.vue';
+import FormComponent from './components/form.vue';
 
 export default {
 
   name: 'App',
-  components:{
+  components: {
     FormComponent
   },
-  data(){
+  data() {
     return {
       customersList: [{
         customerId: '1',
         name: 'Yusuf',
         lastname: 'ELIK',
-        age:34,
+        age: 34,
         email: 'ysfelik1@gmail.com',
         isDisable: false,
       }
@@ -40,32 +40,32 @@ export default {
         customerId: '2',
         name: 'John',
         lastname: 'Doe',
-        age:55,
+        age: 55,
         email: 'jdoe@gmail.com',
         isDisable: false,
-      }   
-       ,
+      }
+        ,
       {
         customerId: '3',
         name: 'Elon',
         lastname: 'Musk',
-        age:54,
+        age: 54,
         email: 'emusk@gmail.com',
         isDisable: false,
       }
-      ,
+        ,
       {
         customerId: '4',
         name: 'Bill',
         lastname: 'Gates',
-        age:64,
+        age: 64,
         email: 'bgates@gmail.com',
         isDisable: false,
       }
       ]
     }
   }
-  
+
 }
 </script>
 
@@ -78,4 +78,16 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+
+.list {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 1rem;
+  margin: 1rem;
+
+}
+
+.list div {
+  padding: 1rem 1rem;
+}</style>
