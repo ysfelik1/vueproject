c558339f3b5507d02cc526ede1ef96295b4f1101
Yusuf Elik <ysfelik1@gmail.com>
2023-10-19 00:20:07 +0200

    gave style

--- hello-world/src/components/form.vue
+++ hello-world/src/components/form.vue
@@ -1,55 +1,58 @@
 <template>
   <div>
     <pre>
-      {{ JSON.stringify(formValues,null,2) }}
+      {{ JSON.stringify(formValues, null, 2) }}
     </pre>
   </div>
-    <form>
-        <div>
-          <label for="name">Name</label>
-          <input type="text" id="name" v-model="formValues.name">
-        </div>
-        <div>
-          <label for="lastname">Lastname</label>
-          <input type="text" id="lastname" v-model="formValues.lastname">
-        </div>
-         <div>
-          <label for="email">Email</label>
-          <input type="email" id="email" v-model="formValues.email">
-        </div>
-    </form>
-  
+  <form>
+    <div>
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model="formValues.name">
+    </div>
+    <div>
+      <label for="lastname">Lastname</label>
+      <input type="text" id="lastname" v-model="formValues.lastname">
+    </div>
+    <div>
+      <label for="email">Email</label>
+      <input type="email" id="email" v-model="formValues.email">
+    </div>
+  </form>
 </template>
    
    
 <script>
 export default {
   name: 'form-component',
-  data(){
-    return{
-      formValues:{
-        name:'',
-        lastname:'',
-        email:'',
+  data() {
+    return {
+      formValues: {
+        name: '',
+        lastname: '',
+        email: '',
       }
     }
-    },
-    methods:{
+  },
+  methods: {
 
-    },
-  }
+  },
+}
 
 
 </script>
    
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+input,
+label {
+  padding: 5px;
+  margin: 5px;
+
+}
+
+form {
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
 }
 </style>
    
